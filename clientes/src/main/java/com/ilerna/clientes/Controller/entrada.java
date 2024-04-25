0f2d3683be65b0341524608c8c07434064a965d9
mikaditos <160121560+mikaditos@users.noreply.github.com>
2024-04-25 13:01:44 +0200

    holas

--- clientes/src/main/java/com/ilerna/clientes/Controller/entrada.java
+++ clientes/src/main/java/com/ilerna/clientes/Controller/entrada.java
@@ -1,6 +1,5 @@
 package com.ilerna.clientes.Controller;
 //comentario nuevo
-//hola
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
