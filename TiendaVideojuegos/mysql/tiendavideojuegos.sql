981d6addc6de37efc0c390869a43009f538f3a59
Jkxkds <108697221+Jkxkds@users.noreply.github.com>
2024-05-14 09:41:48 +0200

    intento de sacar los nombres de los videojuegos de la bbdd

--- TiendaVideojuegos/mysql/tiendavideojuegos.sql
+++ TiendaVideojuegos/mysql/tiendavideojuegos.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-05-2024 a las 17:40:42
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
+-- Tiempo de generación: 14-05-2024 a las 09:06:21
+-- Versión del servidor: 10.4.11-MariaDB
+-- Versión de PHP: 7.4.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -24,6 +25,26 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `id_cliente` int(11) NOT NULL,
+  `nombre` varchar(20) NOT NULL,
+  `edad` int(11) NOT NULL,
+  `telefono` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `cliente`
+--
+
+INSERT INTO `cliente` (`id_cliente`, `nombre`, `edad`, `telefono`) VALUES
+(1, 'nombre1', 25, '123456789');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `compra`
 --
 
@@ -31,7 +52,7 @@ CREATE TABLE `compra` (
   `id_cliente` int(11) NOT NULL,
   `id_videojuego` int(11) NOT NULL,
   `total` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `compra`
@@ -40,9 +61,35 @@ CREATE TABLE `compra` (
 INSERT INTO `compra` (`id_cliente`, `id_videojuego`, `total`) VALUES
 (1, 1, 50);
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `videojuego`
+--
+
+CREATE TABLE `videojuego` (
+  `id_videojuego` int(11) NOT NULL,
+  `nombre` varchar(20) NOT NULL,
+  `tematica` varchar(20) NOT NULL,
+  `precio` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `videojuego`
+--
+
+INSERT INTO `videojuego` (`id_videojuego`, `nombre`, `tematica`, `precio`) VALUES
+(1, 'nombreVideojuego1', 'accion', 50);
+
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id_cliente`);
 
 --
 -- Indices de la tabla `compra`
@@ -52,15 +99,26 @@ ALTER TABLE `compra`
   ADD KEY `id_videojuego` (`id_videojuego`);
 
 --
--- Restricciones para tablas volcadas
+-- Indices de la tabla `videojuego`
+--
+ALTER TABLE `videojuego`
+  ADD PRIMARY KEY (`id_videojuego`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- Filtros para la tabla `compra`
+-- AUTO_INCREMENT de la tabla `cliente`
 --
-ALTER TABLE `compra`
-  ADD CONSTRAINT `compra_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id`),
-  ADD CONSTRAINT `compra_ibfk_2` FOREIGN KEY (`id_videojuego`) REFERENCES `videojuego` (`id`);
+ALTER TABLE `cliente`
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `videojuego`
+--
+ALTER TABLE `videojuego`
+  MODIFY `id_videojuego` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
